6d15efcaae2f4ef90fb73dfa25d2fa22aaf56b7b
dharmesh <darmesh@iqminds.org>
2022-05-24 02:48:52 +0530

    [ADD]changes

--- openeducat_transportation/models/transportation.py
+++ openeducat_transportation/models/transportation.py
@@ -27,6 +27,31 @@ class OpRoute(models.Model):
             self.driver_id = self.vehicle_id.driver_id.id
 
 
+class OpStudent(models.Model):
+    _inherit = "op.student"
+
+    stop_id = fields.Many2one('op.stop', string='Stop', copy=True)
+
+
+class OpStudentAttendance(models.Model):
+    _name = "op.student.attendance"
+
+    attendance_date = fields.Date(string='Date')
+    route_id = fields.Many2one('op.route', string='Route')
+    active = fields.Boolean(default=True)
+    attendance_line_ids = fields.One2many('op.student.attendance.line', 'attendance_id', string='Student attendance', copy=True)
+
+
+class OpStudentAttendanceLine(models.Model):
+    _name = "op.student.attendance.line"
+
+    attendance_id = fields.Many2one('op.student.attendance', string='Attendance', copy=True)
+    stop_id = fields.Many2one('op.stop', string='Stop', copy=True)
+    student_id = fields.Many2one('op.student', string='Student', copy=True)
+    is_arrived = fields.Boolean(string='Is Arrive', copy=True)
+    is_departure = fields.Boolean(string='Is Departure', copy=True)
+
+
 class OpStop(models.Model):
     _name = "op.stop"
     _description = "Manage Stops"
@@ -36,6 +61,9 @@ class OpStop(models.Model):
     active = fields.Boolean(default=True)
     arrival_time = fields.Float(string='Arrival Time')
     departure_time = fields.Float(string='Departure Time')
+    cost = fields.Float(string='Cost')
     route_id = fields.Many2one('op.route', string='Route')
+    # student_ids = fields.One2many('op.student', string='Students')
+    student_ids = fields.One2many('op.student', 'stop_id', string='Students', copy=True)
 
 
